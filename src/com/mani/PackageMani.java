b868bdf6383e4ee6d4eb5b5269270b71b5f9b6a0
tm <tm@t3>
2018-04-18 11:34:11 +0530

    commit

---
+++ src/com/mani/PackageMani.java
@@ -0,0 +1,10 @@
+package com.mani;
+
+public class PackageMani {
+
+	public static void main(String[] args) {
+		System.out.println("Changes in git hub for the second time using new project");
+
+	}
+
+}
